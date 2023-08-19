d79be9de2d3d505c9b6cc306c36112812db11468
modjordan <mod.jordan@icloud.com>
2023-08-18 22:01:05 -0700

    Adding spriteSheet animations and character.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { Farmer } from './farmer';
 
+document.addEventListener("DOMContentLoaded", function () {
+  var char = new Farmer();
+  char.init();
+});
